4fed55a562a3e2f6a7d0c1e481d4a008961f2e42
Peter Ryszkiewicz <pRizz@users.noreply.github.com>
2019-08-21 21:14:12 -0500

    Implement card flipping

--- src/CardComponent.js
+++ src/CardComponent.js
@@ -6,24 +6,20 @@ export default class CardComponent extends React.Component {
   }
 
   render() {
+    const rotatedTransform = this.props.cardViewModel.flipped ? { transform: 'rotateX(180deg)' } : {}
     return (
-      <div style={{width: 200, height: 250, perspective: 1000, margin: 10}} onClick={this.props.onClick}>
-        <div style={{position: 'relative', width: '100%', height: '100%', transition: 'transform 0.5s', transformStyle: 'preserve-3d'}}>
-          {
-            !this.props.cardViewModel.flipped && <div style={{position: 'absolute', width: '100%', height: '100%', backfaceVisibility: 'hidden', backgroundColor: 'green', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <div style={{width: 150, height: 200, perspective: 1000, margin: 10}} onClick={this.props.onClick}>
+        <div style={{position: 'relative', width: '100%', height: '100%', transition: 'transform 0.5s', transformStyle: 'preserve-3d', ...rotatedTransform}}>
+           <div style={{position: 'absolute', width: '100%', height: '100%', backfaceVisibility: 'hidden', backgroundColor: 'green', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
               <div style={{fontSize: '4em'}}>
                 ?
               </div>
             </div>
-          }
-          {
-            this.props.cardViewModel.flipped && <div style={{position: 'absolute', width: '100%', height: '100%', backfaceVisibility: 'hidden', backgroundColor: 'blue', transform: 'rotateX(20deg)', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-              <div style={{fontSize: '4em'}}>
-                {this.props.cardViewModel.value}
-              </div>
+          <div style={{position: 'absolute', width: '100%', height: '100%', backfaceVisibility: 'hidden', backgroundColor: 'blue', transform: 'rotateX(180deg)', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <div style={{fontSize: '4em'}}>
+              {this.props.cardViewModel.value}
             </div>
-          }
-
+          </div>
         </div>
       </div>
     )
